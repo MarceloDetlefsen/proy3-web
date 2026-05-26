9b74a2d23862f52245393e01c2c8572c546088d5
Marcelo Detlefsen <marcelodetlefsen1a@gmail.com>
2026-05-26 00:43:44 -0600

    Se agrega lint para tener un formato estandar y se agregan los tests a realizar con las Github Actions.

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config"
+
+export default defineConfig([
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+])
